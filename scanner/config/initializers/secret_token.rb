5e18226ecce303008f8dba063e2b2fc8db25c2ef
nomadmtb <kylegluce@gmail.com>
2013-11-04 16:48:15 -0800

    added basic project

---
+++ scanner/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scanner::Application.config.secret_key_base = 'a5f305b8e82c99a40b6eba90376ba1fd5c8d762a5f7ffec25f55ccec728dcf8537c5737bb0dc5890d281662c15e7e6dcaded069294d3f1ced8c8eacfa0c0e0a9'
---
+++ scanner/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Scanner::Application.config.session_store :cookie_store, key: '_scanner_session'
